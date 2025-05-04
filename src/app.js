7989da5ba011f546a6aba5ab5a2d78cbd88ec840
Otavio-Emanoel <otavioemanuel6b@gmail.com>
2025-05-03 22:04:26 -0300

    Correção da criação de usuarios

--- src/app.js
+++ src/app.js
@@ -2,19 +2,45 @@ const express = require('express')
 const path = require('path')
 const bodyparser = require('body-parser')
 const mysql = require("mysql2")
-const usuarios = require('./connection/database')
+const { getUsuarios, createUsuario, connection } = require('./connection/database')
+const urlencoded = require('body-parser/lib/types/urlencoded')
 
 const app = express()
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({extended: true}))
 
 
-app.get('/', (req, res) => {
-    res.render('index', {
-        usuarios: usuarios
-    })
+app.get('/', async (req, res) => {
+    try {
+        const usuarios = await getUsuarios()
+        console.log(usuarios)
+        console.log(req.query);
+        
+        res.render('index', {
+            usuarios: usuarios
+        })
+    } catch (error) {
+        console.error("Erro ao obter os usuarios", error)
+        res.status(500).send('Erro ao acessar o banco de dados')
+    }
+})
+
+app.post('/registro', async (req, res) => {
+    try {
+        const { nome, idade, email } = req.body
+        console.log(nome, idade, email)
+        await createUsuario({ nome, idade, email })
+        res.render('index', {usuarios})
+    } catch (error) {
+        console.error("Erro ao criar usuario", error)
+    }
+
 })
 
 app.listen(3000, () => {
--- src/connection/database.js
+++ src/connection/database.js
@@ -1,26 +1,43 @@
 const mysql = require('mysql2')
+const bodyparser = require('body-parser')
 
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
+    password: '987654310',
     database: 'test',
 });
 
-connection.execute(
-    'SELECT * FROM `table`',
-    function (err, results, fields) {
-        console.log(results);
-        // console.log(fields); 
-        results.forEach(user => {
-            
-            console.log(user.nome)
-            console.log(user.idade)
-            console.log(user.email)
-            console.log('')
-        });
-        const usuarios = results
-    }
+function getUsuarios() {
+    return new Promise((resolve, reject) => {
+        connection.execute(
+            'select * from `table`',
+            (err, results) => {
+                if (err) {
+                    reject(err)
+                } else {
+                    resolve(results)
+                }
+            }
 
-);
+        )
+    })
+}
 
-module.exports = connection
+function createUsuario({nome, idade, email}) {
+    return new Promise((resolve, reject) => {
+        connection.execute(
+            'insert into `table` (nome, idade, email) values (?, ?, ?)',
+            [nome, idade, email],
+            (err, results) => {
+                if (err) {
+                    reject(err)
+                } else {
+                    resolve(results)
+                }
+            }
+        )
+    })
+}
+
+module.exports = {connection, getUsuarios, createUsuario }
